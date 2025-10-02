ac51fe296a9a42b9994e157499a34085c8170fde
Мольков Богдан <87420962+OnEseil@users.noreply.github.com>
2025-10-02 14:29:27 +0300

    Update BackendApplication.java

--- src/main/java/org/example/backend/BackendApplication.java
+++ src/main/java/org/example/backend/BackendApplication.java
@@ -8,9 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
-        FirebaseConfig firebaseConfig = new FirebaseConfig();
-        firebaseConfig.upload();
-        firebaseConfig.download();
         SpringApplication.run(BackendApplication.class, args);
     }
 }
